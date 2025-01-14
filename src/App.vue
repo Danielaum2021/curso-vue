5f586a486f53e54b8156cbc160bfcd0e801caac7
unknown <danielaugbmedia@gmail.com>
2025-01-14 17:16:47 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <Contador></Contador>
+  <ListadeTareas></ListadeTareas>
+</template>
+
+<script setup>
+import Contador from './components/Contador.vue'
+import ListadeTareas from './components/ListadeTareas.vue'
+</script>
+
+<style scoped>
+
+</style>
